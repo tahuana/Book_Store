3f76d15d5ac3ac55dded70276284823cf336a20f
Tahuana <tahuana@gmail.com>
2017-09-12 19:11:12 -0400

    Removing EJS dependency

--- README.md
+++ README.md
@@ -8,13 +8,11 @@
 3. Update the .env file with your correct local information
 4. Install dependencies: `npm install`
 
-
 ## Dependencies
 
 - Node 6.9.5 or above
 - NPM 3.10.10 or above
 - Express 4.15.4 or above
-- EJS 2.5.7 or above
 - Ddotenv 4.0.0 or above
 
 
--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const PORT = process.env.PORT || 3000;
 const express = require('express')
 const app = express()
 
+
 app.get('/', (req, res) => {
-  res.send('This will be the home page')
-})
+  res.send('This will be the home page');
+});
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
